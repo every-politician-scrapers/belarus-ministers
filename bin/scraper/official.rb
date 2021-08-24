61f38f1b41889ce8b8fe987c361fb628c4a26d16
Tony Bowden <tony@tmtm.com>
2021-08-24 12:31:06 +0300

    Also scrape the Deputy Prime Ministers page

--- bin/scraper/official.rb
+++ bin/scraper/official.rb
@@ -4,8 +4,8 @@
 require 'every_politician_scraper/scraper_data'
 require 'pry'
 
-class MemberList
-  class Member
+class MinisterList < CabinetMemberList
+  class Minister < CabinetMemberList::Member
     def name
       noko.text.split('–').last.tidy
     end
@@ -19,7 +19,7 @@ class MemberList
     end
   end
 
-  class Members
+  class Ministers < CabinetMemberList::Members
     def member_items
       super.reject { |mem| mem.name.to_s.empty? }
     end
@@ -27,8 +27,41 @@ class MemberList
     def member_container
       noko.css('.option_3').xpath('.//text()[contains(.,"Minister")]')
     end
+
+    def member_class
+      Minister
+    end
   end
 end
 
-file = Pathname.new 'html/official.html'
-puts EveryPoliticianScraper::FileData.new(file).csv
+class DeputyList < CabinetMemberList
+  class Deputy < CabinetMemberList::Member
+    def name
+      noko.css('p').text.tidy
+    end
+
+    def position
+      noko.css('a img/@alt').text.tidy
+    end
+  end
+
+  class Deputies < CabinetMemberList::Members
+    def member_container
+      noko.css('.ul_text_page li')
+    end
+
+    def member_class
+      Deputy
+    end
+  end
+end
+
+file_dpms = Pathname.new 'html/deputypm.html'
+deputypms = EveryPoliticianScraper::FileData.new(file_dpms, klass: DeputyList::Deputies).csv
+
+file_mins = Pathname.new 'html/official.html'
+# TODO: extend CSV method to allow skipping the header row
+ministers = EveryPoliticianScraper::FileData.new(file_mins, klass: MinisterList::Ministers).send(:rows)
+
+puts deputypms
+puts ministers
